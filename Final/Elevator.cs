71cd044363fb085ac9fed07bb5540417e9742fac
acekings14 <a3davis6094@yahoo.com>
2015-12-08 19:18:16 -0500

    final
    final

---
+++ Final/Elevator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdamDavis_Final_Exam
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private int MaxOccupants;
+        private Passenger[] Occupants;
+        public int totalWeight = 0;
+
+        public Elevator ( double MaxWeight, int MaxOccupants)
+        {
+            this.MaxWeight = MaxWeight;
+            Occupants = new Passenger[MaxOccupants];
+
+        }
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetcurrentWeight()
+        {
+            return totalWeight += passenger.weight(); 
+
+        }
+
+       
+
+
+
+
+    }
+}
---
+++ Final/Passenger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdamDavis_Final_Exam
+{
+    class Passenger
+    {
+        private string name;
+        private double weight; 
+        
+        public Passenger (string name, double weight)
+        {
+            this.name = name;
+            this.weight = weight; 
+
+        }
+
+
+
+
+
+
+    }
+}
